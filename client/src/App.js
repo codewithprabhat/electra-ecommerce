1d450c46b0dbd8e793f9e04bfb8fc4aa2051b1a4
codewithprabhat <codewithprabhat16@gmail.com>
2021-02-07 01:56:38 +0530

    Header component setup and antd design configuration

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import {Route, Switch} from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
+import Header from './components/nav/Header'
 
 const App = () =>  (
+  <>
+    <Header/>
     <Switch>
       <Route path='/' component={Home} exact/>
       <Route path='/register' component={Register}/>
       <Route path='/login' component={Login}/>  
     </Switch>
+  </>  
   );
 
 
